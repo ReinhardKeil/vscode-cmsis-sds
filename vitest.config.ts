74c1ad9ada71b7616cae4ae8ce366fb9cde36cf6
Matthias Hertel <matthias.hertel@arm.com>
2026-03-14 14:11:27 +0100

    Remove Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,11 +5,6 @@ export default defineConfig({
         include: ['test/**/*.test.ts'],
         globals: true,
         testTimeout: 10000,
-        reporters: process.env.CI
-            ? ['default', 'junit']
-            : ['default'],
-        outputFile: process.env.CI
-            ? { junit: 'test-results/vitest-report.xml' }
-            : undefined,
+        reporters: ['default'],
     },
 });
